10d3b3032139781695e586dc5f6c919f78260530
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2021-07-13 09:26:37 +0800

    registration

--- resources/views/registrations/show.blade.php
+++ resources/views/registrations/show.blade.php
@@ -12,11 +12,11 @@
 <div class="jumbotron shadow">
 <h4 class="font-weight-bold text-center text-danger">Important Notice: Please Save your Reference Number!</h4>
 <hr class="my-4">
-<p class="font-weight-bold text-center h1">Regisration Reference Number: </p>
+<p class="font-weight-bold text-center h1">Registration Reference Number: </p>
 <p class="font-weight-light h2 text-center">{{ $registration->reg_ref }}</p>
 <hr class="my-4">
 <p class="font-weight-bold text-center">We sent an email. Please always check your email! Also check your spam incase you did not receive in primary mail.</p>
-<p class="font-weight-bold text-center">This reference number is important. you can use it to check the status of your registration.</p>
+<p class="font-weight-bold text-center">This reference number is important. You can use it to check the status of your registration.</p>
 </div>
 
 
