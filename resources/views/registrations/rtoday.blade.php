e2010ec5e38c4a44a6c9152fc2907c352dd4eeb0
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2021-09-06 09:58:16 +0800

    reports

--- resources/views/registrations/rtoday.blade.php
+++ resources/views/registrations/rtoday.blade.php
@@ -87,6 +87,7 @@
               View
             @endif
             </button>
+            {{ $registration->image }}
             </td>
             <td id="or">{{ $registration->or_no }}</td>
      
