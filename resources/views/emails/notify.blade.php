dc1b3b1626ad65303f03c6f79c8deaca71b08348
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2022-01-19 16:50:29 +0800

    update

--- resources/views/emails/notify.blade.php
+++ resources/views/emails/notify.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # COLEGIO DE DAGUPAN - ONLINE REGISTRATION
 @component('mail::panel')
-We Have { $details['admission'] }} Unadmitted Students.<br>
+We Have {{ $details['admission'] }} Unadmitted Students.<br>
 We Have {{ $details['rtoday'] }} Registrations Today.<br>
 We Have {{ $details['unverified'] }} Unverified Payments.<br>
 We Have {{ $details['notenrolled'] }} Not Yet Enrolled Students.<br>
