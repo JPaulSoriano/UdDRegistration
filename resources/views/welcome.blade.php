812e368ec56b09704292bf2755295b309fba0cc3
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2022-05-30 09:40:35 +0800

    UPDATEMAY302022

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,8 @@
     <div class="row justify-content-center mt-5">
         <div class="col-lg-8 text-center">
             <div class="jumbotron ">
-            <img src="{{ asset('images/logo.png') }}" class="img-responsive center-block d-block mx-auto my-3" style="height: 150px">
-            <h1 class="text-primary font-weight-bold text-center">Colegio de Dagupan</h1>
+            {{-- <img src="{{ asset('images/logo.png') }}" class="img-responsive center-block d-block mx-auto my-3" style="height: 150px"> --}}
+            <h1 class="text-primary font-weight-bold text-center">Universidad de Dagupan</h1>
             <h3 class="text-primary font-weight-bold text-center">Online Registration</h3>
             <hr class="my-4">
             <p class="lead">Please select the service you wish to process.</p>
